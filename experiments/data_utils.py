371fdbf3f8dad44f6059e04e34edad3856ec3af3
Odin Hoff Gardaa <odinhg@gmail.com>
2024-03-15 23:35:40 +0100

    changed some processing to numpy

--- experiments/data_utils.py
+++ experiments/data_utils.py
@@ -25,12 +25,11 @@ def subsample_indices(
         return fpsample.bucket_fps_kdtree_sampling(X, n).astype(np.int64)
 
 
-def build_delaunay_graph(X: torch.Tensor) -> torch.Tensor:
+def build_delaunay_graph(X: np.ndarray) -> torch.Tensor:
     """
     Construct Delaunay graph from a point cloud X. Return edge indices.
     """
-    points = X.cpu().numpy()
-    triangulation = scipy.spatial.Delaunay(points, qhull_options="QJ")
+    triangulation = scipy.spatial.Delaunay(X, qhull_options="QJ")
     faces = torch.from_numpy(triangulation.simplices).t().contiguous()
     edge_index = torch.cat(
         [faces[:2], faces[::2], faces[::3], faces[1:3], faces[1::2], faces[2:4]], dim=1
@@ -43,7 +42,7 @@ def build_delaunay_graph(X: torch.Tensor) -> torch.Tensor:
 
 
 def compute_distance_feature(
-    D: torch.Tensor, edge_index: torch.Tensor, p: float = 2.0
+    D: np.ndarray, edge_index: torch.Tensor, p: float = 2.0
 ) -> torch.Tensor:
     """
     Compute distance based edge features from distance matrix D.
@@ -51,32 +50,28 @@ def compute_distance_feature(
     source_idx, target_idx = edge_index
     edge_distances = D[source_idx, target_idx]
     edge_weight = 1 / (edge_distances + 1) ** p
-    return edge_weight
+    return torch.tensor(edge_weight, dtype=torch.float)
 
 
 def compute_edge_density_feature(
-    D: torch.Tensor, edge_index: torch.Tensor
+    D: np.ndarray, edge_index: torch.Tensor
 ) -> torch.Tensor:
     """
     Compute edge density values for edges. The argument D is the distance matrix from centroids to the larger point cloud. That is, D[i, j] = d(c_i, p_j).
     """
-    _, idx = torch.topk(D, k=2, dim=0, largest=False, sorted=False)
-    idx = idx.numpy()
-    count_close_pts = np.vectorize(lambda e: np.sum(np.all(idx.T == e, axis=1)))
-    t0 = time()
-    edge_densities = count_close_pts(edge_index.numpy().T)# + count_close_pts(edge_index.T.flip(0))
-    t1 = time()
-    print(f"{t1-t0:.5f}")
-    edge_weight = edge_densities / edge_densities.max()#D.shape[0]
-    print(edge_weight)
-    return torch.tensor(edge_weight)
+    # Faster numpy solution (not pretty)
+    idx = np.argpartition(D, 2, axis=0)[:2]
+    pairs, counts = np.unique(idx, axis=1, return_counts=True)
+    count_dict = {tuple(pair): count for pair, count in zip(pairs.T, counts)}
+    edge_densities = np.array([count_dict.get(tuple(e), 0) for e in edge_index.numpy().T])
+    edge_weight = edge_densities / edge_densities.max()
+    return torch.tensor(edge_weight, dtype=torch.float)
+    # TODO: Find a better way to compute the edge density function
     """
+    # Slower PyTorch solution using vmap
     _, idx = torch.topk(D, k=2, dim=0, largest=False, sorted=False)
     count_close_pts = torch.vmap(lambda e: torch.sum(torch.all(idx.T == e, dim=1)))
-    t0 = time()
     edge_densities = count_close_pts(edge_index.T)# + count_close_pts(edge_index.T.flip(0))
-    t1 = time()
-    print(f"{t1-t0:.5f}")
     edge_weight = edge_densities / edge_densities.max()#D.shape[0]
     return edge_weight
     """
@@ -90,15 +85,18 @@ def sample_weighted_delaunay_graph(
     replace: bool = False,
     use_edge_density: bool = True,
 ):
+    """
+    Sample m points from P using farthest point sampling. Return node features (coordinates), edge index for the Delaunay graph, and distance and edge density edge features.
+    """
     # Apply transforms (if any)
     if point_cloud_transforms:
         for transform in point_cloud_transforms:
             P = transform(P)
     # Sample centroids
     C_idx = subsample_indices(P, m, sample_method, replace)
-    C = torch.tensor(P[C_idx], dtype=torch.float)
+    C = P[C_idx]
     # Compute distances from centroids to all points
-    D = torch.cdist(C, torch.tensor(P, dtype=torch.float))
+    D = scipy.spatial.distance.cdist(C, P)
     edge_index = build_delaunay_graph(C)
 
     edge_distance_feature = compute_distance_feature(D[:, C_idx], edge_index)
@@ -107,11 +105,7 @@ def sample_weighted_delaunay_graph(
         edge_attr = torch.stack([edge_distance_feature, edge_density_feature], dim=-1)
     else:
         edge_attr = edge_distance_feature.unsqueeze(-1)
+    
+    x = torch.tensor(C, dtype=torch.float)
 
-
-for _ in range(100):
-    t0 = time()
-    P = np.random.uniform(0, 1, (10000, 3))
-    sample_weighted_delaunay_graph(P, 512)
-    t1 = time()
-    print(f"{t1-t0:.5f}")
+    return x, edge_index, edge_attr
